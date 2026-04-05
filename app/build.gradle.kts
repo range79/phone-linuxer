0a341ae0945989c9cbbebda6352373c99428a8c9
range79 <darkrange006@gmail.com>
2026-04-05 23:40:19 +0400

    actions added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         testApplicationId = "com.range.rangeEmulator.test"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
